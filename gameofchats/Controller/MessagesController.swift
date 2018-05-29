71c4d8593ba91a8596ae5280891dd38f76adead1
Casey Henderson <henderson.caseyw@gmail.com>
2018-05-28 20:02:11 -0500

    Created NewMessageController

---
+++ gameofchats/Controller/MessagesController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by Casey Henderson on 5/18/18.
+//  Copyright © 2018 Casey Henderson. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MessagesController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(handleNewMessage))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        checkIfUserIsLoggedIn()
+    }
+    
+    @objc func handleNewMessage() {
+        let newMessageController = NewMessageController()
+        let navController = UINavigationController(rootViewController: newMessageController)
+        present(navController, animated: true, completion: nil)
+    }
+    
+    func checkIfUserIsLoggedIn() {
+        if Auth.auth().currentUser?.uid == nil {
+            handleLogout()
+        } else {
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dictionary["name"] as? String
+                }
+                
+            }, withCancel: nil)
+        }
+    }
+    
+    @objc func handleLogout() {
+        
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        navigationItem.title = nil
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
+
---
+++ gameofchats/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  gameofchats
+//
+//  Created by Casey Henderson on 5/28/18.
+//  Copyright © 2018 Casey Henderson. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    @objc var email: String?
+    @objc var name: String?
+}
