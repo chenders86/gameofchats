e39c7c1b789bd56096094983d8d83e03918b0b9c
Casey Henderson <henderson.caseyw@gmail.com>
2018-06-03 22:50:45 -0500

    Enabled downloading of user profil images and enabled image caching

---
+++ gameofchats/Helpers/Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  Extensions.swift
+//  gameofchats
+//
+//  Created by Casey Henderson on 6/3/18.
+//  Copyright © 2018 Casey Henderson. All rights reserved.
+//
+
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async {
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                    
+                    self.image = downloadedImage
+                }
+            }
+            
+            }.resume()
+    }
+}
